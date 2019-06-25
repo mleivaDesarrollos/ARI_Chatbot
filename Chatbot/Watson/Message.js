d0f6a228b4460727be78989a75d3c26355a97c69
Maximiliano Leiva <maximilianodanielleiva@gmail.com>
2019-06-25 10:18:12 -0300

    Corregido error en el cual la propiedad de contexto 'Require_Workstation' viajaba al ticket

--- Chatbot/Watson/Message.js
+++ Chatbot/Watson/Message.js
@@ -301,9 +301,14 @@ var CheckWorkstationRequirementAndRetrieveMessageWithWorkstationList = async ({ 
         message_with_workstations.options = arrOptions;
         // Empujamos el mensaje filtrado al arrMessages
         filtered_messages.push(message_with_workstations);
+        // Eliminamos la propiedad de requerimiento de máquina del contexto
+        delete caller_context[GET_WORKSTATION_NUMBER];
     }
     // Devolvemos el listado de mensajes procesados
-    return filtered_messages;
+    return {
+        context: caller_context,
+        messages: filtered_messages
+    };
 }
 
 var retrieveUserLocationListOnContextRequest = async({ caller_context, authorization, filtered_messages }) => {
